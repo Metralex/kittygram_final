86708a893b6085123c026b2ab6022539129a986d
metralex <art010600@yandex.ru>
2025-09-30 13:11:39 +0300

    security tuning in .env and settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,13 +2,19 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+from django.core.management.utils import get_random_secret_key
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['localhost', '62.84.120.171', '0.0.0.0:8000', '127.0.0.1', 'showmethecat.hopto.org', 'catexcibition.ddns.net']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -120,7 +126,4 @@ REST_FRAMEWORK = {
 
 }
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://showmethecat.hopto.org",
-    "https://catexcibition.ddns.net",
-]
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
